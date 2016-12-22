338c6856b9abc366124e0d4fe49cd2d8e3d8f87d
Achim Taeschner <ataeschner@LeBit.net>
2016-12-22 09:44:17 +0100

    Init

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { SimpleHTTPComponent } from './simple-http/simple-http.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SimpleHTTPComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
